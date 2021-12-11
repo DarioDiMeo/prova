2f944c83d71f12ca3944e9a3cf7916a9a5a6b96e
DarioDiMeo <95980893+DarioDiMeo@users.noreply.github.com>
2021-12-11 20:31:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 if __name__ == "__main__":
   print("Hello world")
   print("From new pull request")
+<<<<<<< HEAD
   print("Made by Dario Di Meo")
+=======
+>>>>>>> 68fe4ee (ada)
+  print("esempio")
