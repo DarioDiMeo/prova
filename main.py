b0276020a0d6f0891e7412ea9495fd70ad7fdc51
dario di meo <dariodimeo9999@gmail.com>
2021-12-12 19:17:30 +0100

    Nuova stampa

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
 if __name__ == "__main__":
-  print("Hello world")
-  print("From new pull request")
-  print("esempio")
-  print("Made by Dario Di Meo")
-  #Commento prova
-  #Commento from master
+    print("Hello world")
+  print("Stampa")
